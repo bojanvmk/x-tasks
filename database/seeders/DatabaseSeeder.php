950b9f954b45715c70244b65d711a5ec182dd8b8
Bojan <bojan.v.mk@gmail.com>
2022-10-05 21:28:23 +0200

    2. Seed users with roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,37 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $roles = Role::factory()->createMany([
+            [
+                'name' => 'admin',
+            ],
+            [
+                'name' => 'user'
+            ]
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->createMany([
+            [
+                'name'     => 'Admin',
+                'role_id'  => $roles->firstWhere('name', 'admin')->getKey(),
+                'email'    => 'a@abc.org',
+                'password' => Hash::make('Secret!'),
+            ],
+            [
+                'name'     => 'User',
+                'role_id'  => $roles->firstWhere('name', 'user')->getKey(),
+                'email'    => 'b@abc.org',
+                'password' => Hash::make('Secret!'),
+            ],
+        ]);
     }
 }
